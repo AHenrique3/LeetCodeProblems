3a29d230b909150a158fad9141b6020a57c5efb8
Afonso Henrique <afonsohenrique45@gmail.com>
2024-10-09 20:21:41 -0300

    Add 09/10 Challenge and P6

---
+++ Problems/P6_Zigzag_Conversion.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Text;
+
+namespace Problems
+{
+    public class P6_Zigzag_Conversion
+    {
+        public static void RunTests()
+        {
+            var res = Convert("PAYPALISHIRING", 3);
+            Utilities.Output.Print("Test Case 1: ");
+            Utilities.Output.PrintLine(res);
+
+            res = Convert("PAYPALISHIRING", 4);
+            Utilities.Output.Print("Test Case 2: ");
+            Utilities.Output.PrintLine(res);
+
+            res = Convert("AB", 1);
+            Utilities.Output.Print("Test Case 8: ");
+            Utilities.Output.PrintLine(res);
+        }
+
+        public static string Convert(string s, int numRows)
+        {
+            if(numRows < 2)
+                return s;
+
+            var numCols = s.Length;
+            var mat = new char[numRows, numCols];
+            var row = 0;
+            var col = 0;
+            var rowAcc = 1;
+            var colAcc = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                mat[row, col] = s[i];
+
+                if (row == numRows - 1)
+                {
+                    rowAcc = -1;
+                    colAcc = 1;
+                }
+                else if (row == 0 && col > 0)
+                {
+                    rowAcc = 1;
+                    colAcc = 0;
+                }
+
+                row += rowAcc;
+                col += colAcc;
+            }
+
+            var result = new StringBuilder(s.Length);
+            for (int i = 0; i < numRows; i++)
+            {
+                for (int j = 0; j < numCols; j++)
+                    if (mat[i, j] != 0)
+                        result.Append(mat[i, j]);
+            }
+
+            return result.ToString();
+        }
+    }
+}
---
+++ Problems/P921_Minimum_Add_to_Make_Parentheses_Valid.cs
@@ -0,0 +1,35 @@
+﻿namespace Problems
+{
+    public class P921_Minimum_Add_to_Make_Parentheses_Valid
+    {
+        public static void RunTests()
+        {
+            Utilities.Output.Print("Test Case 1: ");
+            Utilities.Output.PrintLine(MinAddToMakeValid("())"));
+
+            Utilities.Output.Print("Test Case 45: ");
+            Utilities.Output.PrintLine(MinAddToMakeValid("()))(("));
+        }
+
+        public static int MinAddToMakeValid(string s)
+        {
+            var result = 0;
+            var counter = 0;
+            foreach (var c in s)
+            {
+                if (c == '(')
+                {
+                    counter++;
+                }
+                if (c == ')')
+                {
+                    if(counter == 0)
+                        result++;
+                    else
+                        counter--;
+                }
+            }
+            return result+counter;
+        }
+    }
+}
