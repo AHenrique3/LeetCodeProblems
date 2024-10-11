ab5574cb1f8902ba1c9931f6fb71a15a621ea435
Afonso Henrique <afonsohenrique45@gmail.com>
2024-10-10 21:14:55 -0300

    P962 - Improving performance and readability

--- Problems/P962_Maximum_Width_Ramp.cs
+++ Problems/P962_Maximum_Width_Ramp.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+﻿using System.Linq;
 
 namespace Problems
 {
@@ -12,69 +12,20 @@ namespace Problems
 
         public static int MaxWidthRamp(int[] nums)
         {
-            var st = new List<Node>();
-
             var maxRampWidth = 0;
-            for (int numsIndex = 0; numsIndex < nums.Length; numsIndex++)
+            var prevIndex = -1;
+            foreach (var tup in nums.Select((num, index) => (num, index)).OrderBy(tup => tup.num))
             {
-                var pos = SortedAdd(st, new Node(nums[numsIndex], numsIndex));
-                for (int j = pos - 1; j >= 0; j--)
-                {
-                    var rampWidth = numsIndex - st[j].Index;
-                    if (rampWidth > maxRampWidth)
-                        maxRampWidth = rampWidth;
-                }
+                if (prevIndex == -1)
+                    prevIndex = tup.index;
+
+                var width = tup.index - prevIndex;
+                if (width > 0 && width > maxRampWidth)
+                    maxRampWidth = width;
+                if (tup.index < prevIndex)
+                    prevIndex = tup.index;
             }
             return maxRampWidth;
-        }
-
-        public static int SortedAdd(List<Node> source, Node item)
-        {
-            int index = source.BinarySearch(item, Node.Comp);
-            if (index < 0)
-            {
-                index = ~index;
-            }
-            
-            source.Insert(index, item);
-            return index;
-        }
-
-        public class Node
-        {
-            public int Value;
-            public int Index;
-            public static readonly IComparer<Node> Comp = new NodeComparer();
-
-            public Node(int value, int index)
-            {
-                Value = value;
-                Index = index;
-            }
-
-            public override string ToString() => $"(\t{Value},\t{Index})";
-        }
-
-        public class NodeComparer : IComparer<Node>
-        {
-            public int Compare(Node? x, Node? y)
-            {
-                if (ReferenceEquals(x, y)) return 0;
-                if (x == null) return -1;
-                if (y == null) return 1;
-
-                if (x.Value < y.Value)
-                    return -1;
-                if (x.Value == y.Value)
-                {
-                    if (x.Index < y.Index)
-                        return -1;
-                    if (x.Index > y.Index)
-                        return 1;
-                    return 0;
-                }
-                return 1;
-            }
         }
     }
 }
