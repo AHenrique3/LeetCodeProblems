4f3ecb7c52be293212911af3c5e7edfa1cf52f1d
Afonso Henrique <afonsohenrique45@gmail.com>
2024-10-24 11:52:34 -0300

    2024-10-24 Challenge

---
+++ Problems/CommonClasses/TreeNode.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+
+namespace Problems.CommonClasses
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode? left;
+        public TreeNode? right;
+        public TreeNode(int val = 0, TreeNode? left = null, TreeNode? right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+        public override string ToString()
+        {
+            return $"{val} L({left} R({right}))";
+        }
+
+        public static TreeNode? CreateTestCase(int?[] sample)
+        {
+            if (sample.Length == 0)
+                return null;
+
+            var root = new TreeNode(sample[0]!.Value);
+            var i = 1;
+            var parents = new List<TreeNode?> { root };
+            var newParents = new List<TreeNode?>(2);
+            while (i < sample.Length)
+            {
+                for (var j = 0; j < parents.Count; j++)
+                {
+                    var parent = parents[j];
+
+                    var sampleValL = i < sample.Length ? sample[i++] : null;
+                    var sampleValR = i < sample.Length ? sample[i++] : null;
+                    if (parent != null)
+                    {
+                        if (sampleValL != null)
+                            parent.left = new TreeNode(sampleValL.Value);
+                        if (sampleValR != null)
+                            parent.left = new TreeNode(sampleValR.Value);
+                    }
+                    newParents.Add(parent?.left);
+                    newParents.Add(parent?.right);
+                }
+                (parents, newParents) = (newParents, parents);
+                newParents.Clear();
+            }
+
+            return root;
+        }
+    }
+}
---
+++ Problems/P951.cs
@@ -0,0 +1,108 @@
+﻿using Problems.CommonClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problems
+{
+    public class P951
+    {
+        public static void RunTests()
+        {
+            Utilities.Output.Print("Test Case 1: ");
+            Utilities.Output.PrintLine(
+                FlipEquiv(
+                    new TreeNode(5,
+                        new TreeNode(4,
+                            new TreeNode(1),
+                            new TreeNode(10)
+                        ),
+                        new TreeNode(9,
+                            null,
+                            new TreeNode(7)
+                        )
+                    ),
+                    new TreeNode(5,
+                        new TreeNode(4,
+                            new TreeNode(1),
+                            new TreeNode(10)
+                        ),
+                        new TreeNode(9,
+                            null,
+                            new TreeNode(7)
+                        )
+                    )
+                )
+            );
+            Utilities.Output.Print("Test Case 2: ");
+            Utilities.Output.PrintLine(
+                FlipEquiv(
+                    TreeNode.CreateTestCase([1, 2, 3, 4, 5, 6, null, null, null, 7, 8]),
+                    TreeNode.CreateTestCase([1, 3, 2, null, 6, 4, 5, null, null, null, null, 8, 7])
+                )
+            );
+            Utilities.Output.Print("Test Case 3: ");
+            Utilities.Output.PrintLine(
+                FlipEquiv(
+                    TreeNode.CreateTestCase([]),
+                    TreeNode.CreateTestCase([1])
+                )
+            );
+        }
+
+        public static bool FlipEquiv(TreeNode? root1, TreeNode? root2)
+        {
+            if (root1 == null && root2 == null)//Fast short circuit
+                return true;
+            if (root1?.val != root2?.val)//Fast short circuit
+                return false;
+
+            return Backtrack([root1], [root2]);
+        }
+
+        public static bool Backtrack(List<TreeNode?> parents1, List<TreeNode?> parents2)
+        {
+            var newParents1 = new List<TreeNode?>(parents1.Count);
+            var newParents2 = new List<TreeNode?>(parents2.Count);
+
+            while (parents1.Count > 0 && parents2.Count > 0)
+            {
+                if (parents1.Count != parents2.Count)
+                    return false;
+
+                for (int i = 0; i < parents1.Count; i++)
+                {
+                    var parent1 = parents1[i];
+                    var parent2 = parents2[i];
+
+                    if (parent1?.left?.val == parent2?.left?.val && parent1?.right?.val == parent2?.right?.val)
+                    {
+                        newParents1.Add(parent1?.left);
+                        newParents1.Add(parent1?.right);
+                        newParents2.Add(parent2?.left);
+                        newParents2.Add(parent2?.right);
+                    }
+                    else if (parent1?.left?.val == parent2?.right?.val && parent1?.right?.val == parent2?.left?.val)
+                    {
+                        newParents1.Add(parent1?.left);
+                        newParents1.Add(parent1?.right);
+                        newParents2.Add(parent2?.right);
+                        newParents2.Add(parent2?.left);
+                    }
+                    else
+                        return false;
+                }
+
+                if (newParents1.All(x => x == null) && newParents2.All(x => x == null))
+                    break;
+
+                (parents1, newParents1) = (newParents1, parents1);
+                newParents1.Clear();
+                (parents2, newParents2) = (newParents2, parents2);
+                newParents2.Clear();
+            }
+
+            return true;
+        }
+    }
+}
